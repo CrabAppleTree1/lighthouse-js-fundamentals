c475b211d3dc0787f2d15f7607f5568d9ac4345e
haiming <hesmaeilz01@gmail.com>
2021-08-26 13:54:33 -0400

    why did the function cross the road;  to get better returns

---
+++ odd.js
@@ -0,0 +1,15 @@
+const isOdd = function(num){
+
+  if(num % 2 == 0){
+    return false;
+  }
+  else {
+    return true;
+  }
+
+}
+console.log(isOdd(2));
+console.log(isOdd(13));
+console.log(isOdd(7));
+console.log(isOdd(1));
+console.log(isOdd(15));
