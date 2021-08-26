41ccca05755d86bc5785d34ad44232f6b556e787
haiming <hesmaeilz01@gmail.com>
2021-08-26 13:43:10 -0400

    todays commit, looks good functionally

---
+++ hello.js
@@ -0,0 +1,9 @@
+const sayHello  = function (name) {
+  console.log("Hello, " + name + ", smile for the world to smile back.");
+}
+
+
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
